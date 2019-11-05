fd9c57a2587b2d7cfd8851ede70051e93326ed6d
andre <andreabonatti92@gmail.com>
2019-11-05 10:16:31 +0100

    Fix Need to try the ocr with text with the char that i need 1 2 3 4 5 6 7 8 9 0 A B C D E F G H I

--- venv/main.py
+++ venv/main.py
@@ -23,11 +23,11 @@ def main():
     # print("Switch to Bcap")
 
     utility.move_to_photo_position(client, hRobot)
-    # image = utility.take_img()
-    # cv.imshow("Image", image)
-    # cv.waitKey()
-    # cv.destroyWindow("Image")
-    # text = utility.tesseract_ocr(image)
+    image = utility.take_img()
+    cv.imshow("Image", image)
+    cv.waitKey()
+    cv.destroyWindow("Image")
+    text = utility.tesseract_ocr(image)
     utility.move_to_the_highligther(client, hRobot)
 
     # switch to use the gripper
@@ -39,10 +39,10 @@ def main():
 
     utility.move_to_initial_writing_position(client, hRobot)
     # Time to put a sheet under the robot
-    # time.sleep(20)
+    time.sleep(5)
 
     # utility.write_a_character("H", client, hRobot)
-    utility.write_a_word("EFE", client, hRobot)
+    utility.write_a_word(text, client, hRobot)
 
     utility.go_up(client, hRobot)
     utility.replace_the_highlighter(client, hRobot)
