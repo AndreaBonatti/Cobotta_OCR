fdd7ba120529aedbef6f0ce300e099b1bf2fa3d8
andre <andreabonatti92@gmail.com>
2019-12-09 15:20:09 +0100

    Deleted some unused robot functions

--- venv/pybcapclient/robot_functions.py
+++ venv/pybcapclient/robot_functions.py
@@ -2,102 +2,14 @@ import io
 import os
 
 import cv2
-import math
 import numpy as np
 import pytesseract
 import win32com.client
 import win32com.client
 from PIL import Image
-from numpy import pi
 
 import pybcapclient.bcapclient as bcapclient
 from pybcapclient.bcapclient import BCAPClient
-
-PIX_MM_RATIO = 9.222
-INVERT = -1
-
-CAMERA_ROBOT_DISTANCE = 52.38570925983608  # mm
-OFFSET_CONF = 0.25  # mm
-CAMERA_ROBOT_DISTANCE_Y = 0.9874772462474191  # mm
-
-
-def polar_to_robot_coordinates(angle, robot_x, robot_y, module=CAMERA_ROBOT_DISTANCE):
-    offset_x = module * math.cos(math.radians(angle))
-    offset_y = -module * math.sin(math.radians(angle))
-    return robot_x + offset_x, robot_y + offset_y
-
-
-def pixels_to_cartesian(img_x, img_y, width=1920, height=1080):  # per coordinate di opencv, non numpy
-    cartesian_x = img_x - width / 2.0
-    cartesian_y = -img_y + height / 2.0
-    return cartesian_x, cartesian_y
-
-
-def find_polar_coordinates(angle, camera_x, camera_y):
-    (x, y) = pixels_to_cartesian(camera_x, camera_y)
-    if np.absolute(x) > math.ceil(PIX_MM_RATIO):
-        L2 = math.sqrt(x ** 2 + y ** 2) / PIX_MM_RATIO
-        alpha2 = math.atan2(y, x) * (180.0 / pi)
-
-        if x > 0 and y < 0:
-            alpha3 = 90 - np.absolute(alpha2)
-            L3 = math.sqrt(
-                CAMERA_ROBOT_DISTANCE ** 2 + L2 ** 2 - 2 * L2 * CAMERA_ROBOT_DISTANCE * math.cos(math.radians(alpha3)))
-            alpha4 = math.degrees(math.asin((float(L2) / L3) * (math.sin(math.radians(alpha3)))))
-            alpha5 = angle + alpha4
-
-        if x > 0 and y >= 0:
-            alpha3 = 90 + alpha2
-            L3 = math.sqrt(
-                CAMERA_ROBOT_DISTANCE ** 2 + L2 ** 2 - 2 * L2 * CAMERA_ROBOT_DISTANCE * math.cos(math.radians(alpha3)))
-            alpha4 = math.degrees(math.asin((float(L2) / L3) * (math.sin(math.radians(alpha3)))))
-            alpha5 = angle + alpha4
-
-        if x < 0 and y >= 0:
-            alpha3 = 360 - (np.absolute(alpha2) + 90)
-            L3 = math.sqrt(
-                CAMERA_ROBOT_DISTANCE ** 2 + L2 ** 2 - 2 * L2 * CAMERA_ROBOT_DISTANCE * math.cos(math.radians(alpha3)))
-            alpha4 = math.degrees(math.asin((float(L2) / L3) * (math.sin(math.radians(alpha3)))))
-            alpha5 = angle - alpha4
-
-        if x < 0 and y < 0:
-            alpha3 = np.absolute(alpha2 + 90)
-            L3 = math.sqrt(
-                CAMERA_ROBOT_DISTANCE ** 2 + L2 ** 2 - 2 * L2 * CAMERA_ROBOT_DISTANCE * math.cos(math.radians(alpha3)))
-            alpha4 = math.degrees(math.asin((float(L2) / L3) * (math.sin(math.radians(alpha3)))))
-            alpha5 = angle - alpha4
-
-        return (L3, alpha5)
-
-    else:
-        L3 = CAMERA_ROBOT_DISTANCE + (y / PIX_MM_RATIO)
-        return (L3, angle)
-
-
-def find_orientation(contour, robot_angle):
-    (_, _), (_, _), angle = cv2.fitEllipse(contour)
-    new_angle = 0
-
-    if angle <= 90:
-        if robot_angle <= 0:
-            new_angle = robot_angle + angle
-        elif robot_angle > 0 and angle >= 50:
-            beta = 90 - angle
-            gamma = 90 - robot_angle
-            new_angle = -(gamma + beta)
-        elif robot_angle > 0 and angle < 50:
-            new_angle = robot_angle + angle
-    else:
-        if robot_angle > 0:
-            beta = 180 - angle
-            new_angle = robot_angle - beta
-        elif robot_angle <= 0 and angle <= 160:
-            new_angle = robot_angle + angle
-        elif robot_angle <= 0 and angle > 160:
-            beta = 180 - angle
-            new_angle = robot_angle - beta
-
-    return new_angle
 
 
 def connect(host, port, timeout, provider="CaoProv.DENSO.VRC"):
